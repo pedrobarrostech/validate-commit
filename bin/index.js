3db51d8b134c71aea2a7ba5b55939510f3fd5763
Will Soto <will.soto9@gmail.com>
2016-03-09 19:35:58 -0500

    feat: add ability to pass a preset to the cli
    Also refactors the CLI so it doesn't need a hacky start

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var isFile = require('is-file');
 
 var pkg = require('../package.json');
 
@@ -11,20 +12,23 @@ program
   .version(pkg.version)
   .description(pkg.description)
   .command('validate-commit-msg <message>', 'validate a message')
+  .option('-p, --preset <preset>', 'specify a preset (angular|atom) [angular]', 'angular')
   .action(function(message) {
-    // hacky start if this is being used as a git hook
-    // it will read in the message from a file instead of the command line
-    if (process.argv.join('').indexOf('.git') > -1) {
-      if (!validateMessageFromBuffer(message)) {
-        process.exit(1);
-      }
-      process.exit(0);
+    var valid = false;
+    var options = {
+      preset: program.preset
+    };
+
+    if (isFile(message)) {
+      valid = validateMessageFromBuffer(message, options);
     } else {
-      if (!validateMessage(message)) {
-        process.exit(1);
-      }
-      process.exit(0);
+      valid = validateMessage(message, options);
     }
+
+    if (valid === false) {
+      process.exit(1);
+    }
+    process.exit(0);
   });
 
 program.parse(process.argv);
